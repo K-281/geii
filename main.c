eb31ea844df9c10fb805bc6cbbfadff470049bf4
K-281 <48651626+K-281@users.noreply.github.com>
2022-10-12 09:36:42 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+int n,i,f=1;
+printf("donnez votre factorielle n: ");
+scanf("%d", n);
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+printf("\nLe resultat est : %d", f);
+
+
+
+
+
+    return 0 ;
